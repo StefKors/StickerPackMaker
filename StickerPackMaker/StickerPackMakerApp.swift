7dd3a87d4d706dbade11852482a0537f7ece26b9
Stef Kors <stef.kors@gmail.com>
2023-11-12 21:03:36 +0100

    Initial Commit

---
+++ StickerPackMaker/StickerPackMakerApp.swift
@@ -0,0 +1,32 @@
+//
+//  StickerPackMakerApp.swift
+//  StickerPackMaker
+//
+//  Created by Stef Kors on 12/11/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StickerPackMakerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
